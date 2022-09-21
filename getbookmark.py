8334d0f9da82dd5853992f8876caed00fc92e42e
乌鸦已隐居 <xiucai315@163.com>
2022-09-21 14:28:16 +0800

    Update getbookmark.py
    增加文件写回功能

--- getbookmark.py
+++ getbookmark.py
@@ -15,8 +15,27 @@ except AttributeError:
     # no pyopenssl support used / needed / available
     pass
 
+fpmark=open("d:/bookmars_sort.html",'w',encoding="utf8")
 
-fp=open("d:/bookmarks_2022_9_18.html",'r',encoding="utf8")
+fpmark.write("<!DOCTYPE NETSCAPE-Bookmark-file-1>\n")
+
+fpmark.write("<META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=UTF-8\">\n")
+
+fpmark.write("<TITLE>Bookmarks</TITLE>\n")
+
+fpmark.write("<H1>Bookmarks</H1>\n")
+
+fpmark.write("<DL><p>\n")
+
+fpmark.write("<DT><H3 ADD_DATE=\"1562840722\" LAST_MODIFIED=\"1649945997\" PERSONAL_TOOLBAR_FOLDER=\"true\">书签栏</H3>\n")
+
+fpmark.write("<DL><p>\n")
+
+
+
+
+
+fp=open("d:/bookmarks_2022_9_21.html",'r',encoding="utf8")
 text=fp.read()
 fp.close()
 soup=BeautifulSoup(text,'lxml')
@@ -39,6 +58,7 @@ for mark in allmarked:
 		rst=requests.get(mark.get('href'),headers=headers)
 		if rst.ok:
 			print(mark.get('href'))
+			fpmark.write("<DT><A HREF=\""+mark.get('href')+"\">"+mark.text+"</A>\n")
 		else:
 			failed=failed+1
 	except requests.exceptions.ConnectionError as e:
@@ -47,6 +67,10 @@ for mark in allmarked:
 print("p count:"+str(len(allmarked)-failed))
 
 
+
+fpmark.write("</DL><p>\n")
+
+fpmark.close()
 #将有效的url重新写回bookmark
 
 
